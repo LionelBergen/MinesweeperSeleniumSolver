f8a7dcb7abad7cc7881dba02a12187ae756a4d77
unknown <lionelBergen@live.com>
2019-11-25 12:25:51 -0600

    sectionanalyzer

--- Solver/src/main/java/solver/board/analyzing/SectionAnalyzer.java
+++ Solver/src/main/java/solver/board/analyzing/SectionAnalyzer.java
@@ -1,12 +1,22 @@
 package solver.board.analyzing;
 
 import java.util.List;
+import java.util.Set;
 
+import component.model.GameSquare;
 import solver.component.Section;
 import solver.component.SweeperSet;
 
 public class SectionAnalyzer {
-	public List<SweeperSet> breakupSection(Section section) {
+	public static List<SweeperSet> breakupSection(Section section) {
+		Set<GameSquare> squaresInSection = section.getGameSquares();
+		
+		for (GameSquare square : squaresInSection) {
+			if (square.getValue().isNumbered()) {
+				
+			}
+		}
+		
 		return null;
 	}
 }
--- Tests/src/test/java/tests/minesweeper/solver/calculation/SectionAnalyzerTest.java
+++ Tests/src/test/java/tests/minesweeper/solver/calculation/SectionAnalyzerTest.java
@@ -1,10 +1,52 @@
 package tests.minesweeper.solver.calculation;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.junit.Test;
+
+import component.model.GameSquare;
+import component.model.SquareValue;
+import solver.board.analyzing.SectionAnalyzer;
+import solver.component.Section;
+import solver.component.SweeperSet;
+import tests.minesweeper.data.TestScenarios;
+import tests.minesweeper.data.component.TestScenario;
 
 public class SectionAnalyzerTest {
 	@Test
+	public void testEmptySection() {
+		Section section = new Section();
+		
+		List<SweeperSet> results = SectionAnalyzer.breakupSection(section);
+		
+		assertEquals(0, results.size());
+	}
+	
+	@Test
+	public void testBlankSection() {
+		Set<GameSquare> blankSquareSet = new HashSet<GameSquare>();
+		blankSquareSet.add(new GameSquare(SquareValue.BLANK_UNTOUCHED, 0, 0));
+		blankSquareSet.add(new GameSquare(SquareValue.BLANK_UNTOUCHED, 2, 4));
+		blankSquareSet.add(new GameSquare(SquareValue.BLANK_UNTOUCHED, 1, 6));
+		blankSquareSet.add(new GameSquare(SquareValue.BLANK_UNTOUCHED, 4, 4));
+		
+		Section section = new Section(blankSquareSet);
+		
+		List<SweeperSet> results = SectionAnalyzer.breakupSection(section);
+		assertEquals(0, results.size());
+	}
+	
+	@Test
 	public void testScenario01() {
-		//Section sectionToTest TestScen
+		TestScenario testScenario = TestScenarios.SCENARIO_01;
+		
+		Section section = testScenario.getExpectedSections().get(0);
+		
+		List<SweeperSet> results = SectionAnalyzer.breakupSection(section);
+		assertEquals(1, results.size());
 	}
 }
