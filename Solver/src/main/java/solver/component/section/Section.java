87d057c4442b1d91a8a3168eca19bec8346ae1c9
unknown <lionelBergen@live.com>
2019-12-01 21:33:33 -0600

    removed unneeded method

--- Solver/src/main/java/solver/component/section/Section.java
+++ Solver/src/main/java/solver/component/section/Section.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 import component.model.GameSquare;
-import utility.util.Utility;
 
 /**
  * Basically just a Collection<GameSquare>, but with added features
@@ -39,46 +38,5 @@ public class Section extends GenericSection<GameSquare> {
 	@Override
 	public void setGameSquares(Set<GameSquare> gameSquares) {
 		this.gameSquares = gameSquares;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 92821;
-		int hashCode = 1;
-		
-		for (GameSquare square : Utility.sortList(this.gameSquares)) {
-			hashCode = hashCode * prime + square.hashCode();
-		}
-		
-	    return hashCode;
-	}
-	
-	@Override
-	public boolean equals(Object other) {
-	    // self check
-	    if (this == other) {
-	        return true;
-	    }
-	    
-	    // null check
-	    if (other == null) {
-	        return false;
-	    }
-	    
-	    // type check and cast
-	    if (getClass() != other.getClass()) {
-	        return false;
-	    }
-	    
-	    Section otherResultSet = (Section) other;
-	    
-	    boolean sizesAreEqual = this.gameSquares.size() == otherResultSet.gameSquares.size();
-	    
-	    if (!sizesAreEqual) {
-	    	return false;
-	    }
-	    
-	    // Don't care about order
-	    return this.gameSquares.containsAll(otherResultSet.gameSquares);
 	}
 }
