0850284fef68708c8ac4925ebc727ddef59a8600
unknown <lionelBergen@live.com>
2019-12-01 20:06:42 -0600

    removed unused import

--- Solver/src/main/java/solver/component/Section.java
+++ Solver/src/main/java/solver/component/Section.java
@@ -2,7 +2,6 @@ package solver.component;
 
 import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 import component.model.GameSquare;
 import utility.util.Utility;
