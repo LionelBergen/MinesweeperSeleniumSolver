c776d10ced37aecf7b5a50ffd256d616d21e3aa2
unknown <lionelBergen@live.com>
2019-12-02 17:34:42 -0600

    creating program for test scenerios

--- Tests/src/test/java/tests/minesweeper/data/creator/GameBoardTestScenerioCreator.java
+++ Tests/src/test/java/tests/minesweeper/data/creator/GameBoardTestScenerioCreator.java
@@ -1,5 +1,14 @@
 package tests.minesweeper.data.creator;
 
+/**
+ * Creates HTMl, CSS, JS files used to create test scenerios.
+ * 
+ * These scenerios can be used for visualizing, or building automated test cases
+ *  
+ * @author Lionel Bergen
+ */
 public class GameBoardTestScenerioCreator {
-	
+	public static void main(String[] args) {
+		
+	}
 }
