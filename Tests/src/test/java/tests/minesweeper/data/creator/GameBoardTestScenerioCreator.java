6be15b8255e86c51bd37335df932736902001467
unknown <lionelBergen@live.com>
2019-12-02 18:04:14 -0600

    added assets to tests project

--- Tests/src/test/java/tests/minesweeper/data/creator/GameBoardTestScenerioCreator.java
+++ Tests/src/test/java/tests/minesweeper/data/creator/GameBoardTestScenerioCreator.java
@@ -1,5 +1,12 @@
 package tests.minesweeper.data.creator;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
+
 /**
  * Creates HTMl, CSS, JS files used to create test scenerios.
  * 
@@ -8,7 +15,32 @@ package tests.minesweeper.data.creator;
  * @author Lionel Bergen
  */
 public class GameBoardTestScenerioCreator {
-	public static void main(String[] args) {
+	private static final String TEMP_DIRECTORY = System.getProperty("java.io.tmpdir");
+	private static final String OUTPUT_DIRECTORY = TEMP_DIRECTORY + "minesweeper-outputfiles" + File.separator;
+	
+	public static void main(String[] args) throws Exception {
+		final File outputDirectory = new File(OUTPUT_DIRECTORY);
+		deleteFolder(outputDirectory);
+		System.out.println("Deleted: " + outputDirectory);
 		
+		//File htmlFile = System.getd
+		System.out.println(getFileContentsFromAssets("minesweeper.html"));
+	}
+	
+	private static void deleteFolder(File folder) throws IOException {
+		String[] entries = folder.list();
+		
+		if (entries != null) {
+			for(String s: entries){
+			    File currentFile = new File(folder.getPath(), s);
+			    currentFile.delete();
+			}
+		}
+	}
+	
+	private static String getFileContentsFromAssets(String fileName) throws IOException {
+		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName);
+		
+		return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
 	}
 }
