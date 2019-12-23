53d29b1578e281417f5826a717b911937666aeee
unknown <lionelBergen@live.com>
2019-12-23 16:50:44 -0600

    removed some unused classes

--- Tests/src/test/java/tests/minesweeper/solver/data/GameBoardTestData.java
+++ Tests/src/test/java/tests/minesweeper/solver/data/GameBoardTestData.java
@@ -4,10 +4,17 @@ import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+import component.model.RegularGameBoard;
+import component.model.gamesquare.GameSquare;
+import component.model.gamesquare.SquareValue;
 
 public class GameBoardTestData {
 	private static final String DATA_DIRECTORY = "tests/minesweeper/solver/data/";
@@ -16,16 +23,42 @@ public class GameBoardTestData {
 	private static final ClassLoader CLASS_LOADER = new GameBoardTestData().getClass().getClassLoader();;
 	
 	public static void main(String[] args) throws Exception {
-		
+		getTestScenario(SPECIAL_SENARIO_03_FILE_NAME);
+	}
+	
+	private static RegularGameBoard getTestScenario(String fileName) throws Exception {
 		File file = getFile(SPECIAL_SENARIO_03_FILE_NAME);
 		String fileContents = Files.readString(Paths.get(file.getPath()), StandardCharsets.US_ASCII);
 		
 		JSONParser parser = new JSONParser();
 		JSONObject result = (JSONObject) parser.parse(fileContents);
 		
-		//JSONArray jsonArray = 
-		System.out.println(result.get("mines"));
-		System.out.println(result);
+		int numberOfMines = Integer.parseInt(result.get("mines").toString());
+		List<GameSquare> gameSquaresFromResults = new ArrayList<GameSquare>();
+		
+		JSONArray listOfAllSquares = (JSONArray) result.get("squares");
+		
+		for (Iterator<?> iter = listOfAllSquares.iterator(); iter.hasNext();) {
+			JSONObject gameSquare = (JSONObject) iter.next();
+			
+			String name = gameSquare.get("name").toString();
+			SquareValue squareValue = isSingleDigitNumber(name) ? SquareValue.fromValue(name) : SquareValue.BLANK_UNTOUCHED;
+			int x = Integer.parseInt(gameSquare.get("x").toString());
+			int y = Integer.parseInt(gameSquare.get("y").toString());
+			
+			gameSquaresFromResults.add(new GameSquare(name, squareValue, x, y));
+		}
+		
+		System.out.println(numberOfMines);
+		System.out.println(gameSquaresFromResults);
+		
+		RegularGameBoard gameBoard = new RegularGameBoard(gameSquaresFromResults, numberOfMines);
+		
+		return gameBoard;
+	}
+	
+	private static boolean isSingleDigitNumber(String text) {
+		return text != null && !text.isEmpty() && Character.isDigit(text.charAt(0));
 	}
 	
 	private static File getFile(String fileName) {
